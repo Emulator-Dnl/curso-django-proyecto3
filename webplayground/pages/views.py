36f14d2928d2288bf2d03145399ec9221afe4581
Daniel Michel <dmichel@HYDRA.COM>
2023-09-15 16:38:44 -0600

    se añade CRUD de pages utilizando Class Based Views

---
+++ webplayground/pages/views.py
@@ -0,0 +1,29 @@
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Page
+from django.urls import reverse, reverse_lazy
+
+# Create your views here.
+class PageListView(ListView):
+    model = Page
+
+class PageDetailView(DetailView):
+    model = Page
+
+class PageCreateView(CreateView):
+    model = Page
+    fields = ['title', 'content', 'order']
+    success_url = reverse_lazy('pages:pages')
+
+class PageUpdateView(UpdateView):
+    model = Page
+    fields = ['title', 'content', 'order']
+    template_name_suffix = "_update_form"
+
+    def get_success_url(self):
+        return reverse_lazy('pages:update', args=[self.object.id] + '?ok')
+    
+class PageDeleteView(DeleteView):
+    model = Page
+    success_url = reverse_lazy("pages:pages")
